4577d012b4e7951179d4f5c5782a8c58fdfeae05
VijeyRam1987 <vkumar@Arkieva.com>
2015-07-26 19:30:32 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  xinv <- NULL # result of inversion
+  # this is to set a matrix to object created by makeCacheMatrix function
+  set <- function(y) {
+    x <<- y
+    xinv <<- NULL # initialises xinv to null
+  }
+  
+  get <- function() x 
+  setInv <- function(inv) xinv <<- inv 
+  getInv <- function() xinv 
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 
 }
 
@@ -11,5 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv() 
+  if(!is.null(m)) { 
+    message("Cached data being loaded...")
+    return(m) 
+  }
+  data <- x$get() 
+  m <- solve(data) 
+  x$setInv(m) 
+  m 
 }
